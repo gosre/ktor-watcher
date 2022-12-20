d35527330baf512ea42f91b907aef79d20e18079
Clayton Williams <32527177+gosre@users.noreply.github.com>
2022-12-20 14:12:32 -0500

    chore: add kaml and ktor client dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,11 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val krontab_version: String by project
+val kaml_verison: String by project
 
 plugins {
     kotlin("jvm") version "1.7.22"
+    kotlin("plugin.serialization") version "1.7.20"
     id("io.ktor.plugin") version "2.2.1"
 }
 
@@ -22,11 +24,17 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("com.charleskorn.kaml:kaml:$kaml_verison")
     implementation("dev.inmo:krontab:$krontab_version")
+
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
